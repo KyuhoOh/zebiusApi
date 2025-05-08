10aa6ed20b26e6fd024fb1da3e7f6d3bd6dd7a1b
kyuhoOh <kh0595@kakao.com>
2025-05-09 02:00:57 +0900

    .

--- src/routes/zebius.js
+++ src/routes/zebius.js
@@ -14,45 +14,41 @@ const getNanoTime = () => (isDev ? process.hrtime.bigint() : 0n);
 
 export default async function (fastify, opts) {
   fastify.post("/zebius", async (request, reply) => {
-    const t0 = getNanoTime();
     const redis = fastify.redis;
-    try {
-      const t1 = getNanoTime();
-      const { c, r, t, p } = request.body;
+    const t0 = getNanoTime();
+    const { c, r, t, p } = request.body;
+    const t1 = getNanoTime();
+    global.zev = ((global.zev + p - 1) & TLensMask) + 1;
+    const newZev = global.zev;
+    const oldZev = (newZev - p + TLens) & TLensMask;
+    const a = Math.floor(newZev / ratio);
+    const xa = Math.floor(oldZev / ratio);
+    const b = newZev - a;
+    const A = oldZev < newZev ? a - xa : a + ALens - xa;
+    const B = p - A;
+    const t2 = getNanoTime();
+    const z = operateEngine(A, B, a, b);
+    const t3 = getNanoTime();
+    const data = { s: 1, t, c, r, p, z, newZev };
+    writeLog(data, redis);
+    const t4 = getNanoTime();
 
-      const t2 = getNanoTime();
-      global.zev = ((global.zev + p - 1) & TLensMask) + 1;
-      const newZev = global.zev;
-      const oldZev = (newZev - p + TLens) % TLens;
-      const a = Math.floor(newZev / ratio);
-      const xa = Math.floor(oldZev / ratio);
-      const b = newZev - a;
-      const A = oldZev < newZev ? a - xa : a + ALens - xa;
-      const B = p - A;
-
-      const t3 = getNanoTime();
-      const z = operateEngine(A, B, a, b);
-
-      const t4 = getNanoTime();
-      const data = { s: 1, t, c, r, p, z, newZev };
-      writeLog(data, redis);
-      const t5 = getNanoTime();
-
-      if (isDev) {
-        console.log("---- Timing Breakdown ----");
-        console.log("Total Elapsed Time:", (t5 - t0).toString(), "ns");
-        console.log("Request Parse Time:", (t2 - t1).toString(), "ns");
-        console.log("Data Managed Time:", (t3 - t2).toString(), "ns");
-        console.log("Engine Calculation Time:", (t4 - t3).toString(), "ns");
-        console.log("Log Write Time:", (t5 - t4).toString(), "ns");
-      }
-
-      return reply.send({
-        data,
-      });
-    } catch (error) {
-      console.error("Error:", error);
-      return reply.code(500).send({ message: "Failed to request Zebius" });
+    if (isDev) {
+      const lapTime = {
+        total: t4 - t0,
+        t1_t0: t1 - t0,
+        t2_t1: t2 - t1,
+        t3_t2: t3 - t2,
+        t4_t3: t4 - t3,
+      };
+      const lapTimeForJson = Object.fromEntries(
+        Object.entries(lapTime).map(([key, value]) => [key, value.toString()])
+      );
+      data.lapTime = lapTimeForJson;
     }
+
+    return reply.send({
+      ...data,
+    });
   });
 }
